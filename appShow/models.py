61ebdc4fa4290cbb32b57177a63129deb002a6fe
Neven Amen <nevenamen2000@gmail.com>
2022-12-20 15:49:20 +0200

    show TV valiator

--- appShow/models.py
+++ appShow/models.py
@@ -1,12 +1,46 @@
+import datetime
+import time
 from django.db import models
 
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        len_titles = len(Show.objects.filter(title = postData['title']))
+
+        # relase_date = postData['relase_date']
+        # print(relase_date)
+        # local_date = datetime.date.today()
+        # print("local date------")
+        # print(local_date)
+        # print(local_date == datetime.date.fromtimestamp(time.time()))
+        # datee = time.strptime(relase_date, "%Y-%m-%d") 
+        # # local = time.strptime(local_date, "%Y-%m-%d") 
+        # # print(abs(datee - local_date))
+        # print("date------------")
+        # print(datee)
+       
+        print
+        if len(postData['title']) < 3 :
+            errors['title'] = "Show title should be at least 3 characters."
+        if len_titles > 0 :
+            errors['titleunqiue'] = "Show title name must be unique"     
+        if len(postData['network']) < 3 :
+            errors['network'] = "Show network should be at least 3 characters"
+        if ((datetime.datetime.strptime(postData['relase_date'], '%Y-%m-%d')) > (datetime.datetime.today())):
+            errors['relase_date'] = "Show Relase date should brfore local date"
+        if len(postData['desc']) < 5 :
+            errors['desc'] = "Show description should be at least 5 characters" 
+        return errors
+
 class Show(models.Model):
-    title = models.CharField(max_length=60)
+    title = models.CharField(max_length=60,unique=True)
     network = models.TextField(max_length=60)
     desc = models.TextField()
     relase_date = models.DateTimeField(auto_now=False,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
 
 def show_Show_detailes(request):
     return Show.objects.all()
@@ -19,7 +53,7 @@ def create_show(request):
     return Show.objects.create(title = title , network = network,desc = desc , relase_date = relase_date)
 
 def show_show(request):
-    id = request.GET['show_show_id']
+    id = request.POST['show_show_id']
     return Show.objects.filter(id = id)
 
 def edit_show(request):
@@ -33,11 +67,16 @@ def delete_show(request):
 
 def update_show_process(request):
     id = request.POST['edit_show_id']
-    title_edit = request.POST['title_edit']
-    network_edit = request.POST['network_edit']
-    relase_date_edit = request.POST['relase_date_edit']
-    desc_edit = request.POST['desc_edit']
-    show = Show.objects.get(id = id)
+    title_edit = request.POST['title']
+    network_edit = request.POST['network']
+    relase_date_edit = request.POST['relase_date']
+    desc_edit = request.POST['desc']
+    print(id)
+    print(title_edit)
+    print(network_edit)
+    print(relase_date_edit)
+    print(desc_edit)
+    show = Show.objects.get(id = id )
     show.title = title_edit 
     show.network = network_edit
     show.relase_date = relase_date_edit
--- appShow/views.py
+++ appShow/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
 from appShow import models
+from django.contrib import messages
+from .models import Show
+
 
 def show_detailes(request):
     show = models.show_Show_detailes(request)
@@ -10,11 +13,17 @@ def show_detailes(request):
 
 def add_show_page(request):
     return render(request,'addShow.html')
-
+# ------------------------------------------------------------validation
 def add_show_process(request):
-    if request.method == "POST":
-        models.create_show(request)
-    return redirect('/')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors)>0:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect('/add_show_page')
+    else:
+        if request.method == "POST":
+            models.create_show(request)
+        return redirect('/')
 
 def get_show_data(request):
     show = models.show_Show_detailes(request)
@@ -31,7 +40,7 @@ def show_info(request):
     return render(request,'show.html',context)
 
 def show_page(request):
-    if request.method == "GET":
+    if request.method == "POST":
         id = models.show_show(request)
     context = {
         "show" : id[0] 
@@ -44,11 +53,13 @@ def show_page(request):
 def edit_show(request):
     if request.method == "POST":
         id = models.edit_show(request)
+        show_id = id[0].id
         relase_date = id[0].relase_date
         title = id[0].title
         network = id[0].network
         desc = id[0].desc
         context = {
+            "show_id" : show_id,
             "relase_date" : relase_date,
             "title" : title,
             "network" : network,
@@ -62,6 +73,12 @@ def delete_show(request):
     return redirect('/')
 
 def update_show(request):
-    if request.method == "POST":
-        models.update_show_process(request)
-    return redirect('/')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors)>0:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect('/add_show_page')
+    else:
+        if request.method == "POST":
+            models.update_show_process(request)
+        return redirect('/')
